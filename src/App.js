c546d87ebf307dbd7c5cd2630cab0114dc6c7b5d
Volodymyr Lavrentiev <cubesquareromb@gmail.com>
2025-02-08 14:31:50 +0200

    GitHub pages (5)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import React from 'react'
 import './App.css';
+import Drawer from 'react-modern-drawer'
+import 'react-modern-drawer/dist/index.css'
 
 function App() {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const toggleDrawer = () => {
+      setIsOpen((prevState) => !prevState)
+  }
+
   return (
     <div className="App">
+      <>
+          <button onClick={toggleDrawer}>Show</button>
+          <Drawer
+              open={isOpen}
+              onClose={toggleDrawer}
+              direction='left'
+              className='drawer'
+          >
+              <div style={{ padding: "20px", display: "flex", flexDirection: "column", gap: "20px" }}>
+                <h2>Drawer Content</h2>
+                <div onClick={() => alert("Button 1 clicked!")}>Button 1</div>
+                <div onClick={() => alert("Button 2 clicked!")}>Button 2</div>
+                <div onClick={toggleDrawer}>Close Drawer</div>
+              </div>
+          </Drawer>
+      </>
+
       <main>
         <h1>Volodymyr</h1>
         <h3>Flutter & Native Android</h3>
